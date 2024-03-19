13fffefd0276516577d598522d02f2742925e854
adam <snowadam@icloud.com>
2024-03-19 11:41:47 +1100

    Disable speedInsights in astro.config.mjs to fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,15 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
-const VERCEL_ANALYTICS_ID = process.env.VERCEL_ANALYTICS_ID || process.env.PUBLIC_VERCEL_ANALYTICS_ID;
-
-console.log(
-  VERCEL_ANALYTICS_ID,
-  process.env.PUBLIC_VERCEL_ANALYTICS_ID,
-);
-
+ 
 export default defineConfig({
   output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
-      id: VERCEL_ANALYTICS_ID, // Use the ID here
     },
     speedInsights: {
-      enabled: true,
+      enabled: false,
     },
   }),
   integrations: [tailwind()]
